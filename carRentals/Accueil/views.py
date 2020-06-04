d73b024b7f6ff2be0638fa80446b72024fb87edc
afefhnid <afefhnid@gmail.com>
2020-06-04 20:25:19 +0200

    url

--- carRentals/Accueil/views.py
+++ carRentals/Accueil/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
+
+def home(request):
+    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
+    return HttpResponse("""
+    <h1>Votre prochaine voiture vous attend !</h1>
+    <p>Louez une voiture à l'heure ou à la journée!</p>
+    """)
+
+
+def date_actuelle(request):
+    return render(request, 'blog/date.html', {'date': datetime.now()})
 # Create your views here.
--- carRentals/carRentals/settings.py
+++ carRentals/carRentals/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'carRentals.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # Cette ligne ajoute le dossier templates/ à la racine du projet
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
