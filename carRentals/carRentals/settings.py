34a3b4828f756c07d085e8b95920e4495376a6cd
afefhnid <afefhnid@gmail.com>
2020-06-04 21:03:26 +0200

    fixe errer

--- carRentals/carRentals/settings.py
+++ carRentals/carRentals/settings.py
@@ -38,10 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Accueil',
-    'crispy_forms',
-    'bootstrap4'
+
 ]
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
